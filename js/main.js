aefa07805c7c58c8cdc05aa43140aadca4997113
user <mbraden9418@yahoo.com>
2018-03-23 18:17:53 -0500

    js console play

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+var teacher= "Mr. Vincent VanGogh ";
+var department= "Art";
+var rating= [2.1, 3.7, 4.9];
+var avgRating= (rating[0] +rating[1]+rating[2])/3;
+var roundedAvgRating= Math.round(10*avgRating)/10;
+
+console.log("Teacher: ", teacher,
+"\nDepartment: " , department,
+"\nRatings: ", rating,
+"\n Avg Rating", roundedAvgRating);
+
+var courses=[ "Interpersonal Communication 103", "Psychology 101", "Yoga 200"];
+var gpa= 4.0;
+var email= "lovinTrees@yahoo.com";
+console.log("Courses:", courses, "\n GPA:", gpa, "\n Email:", email);
