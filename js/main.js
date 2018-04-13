b8671cec3b86e83899f209e380679d7b10a4e111
user <mbraden9418@yahoo.com>
2018-04-12 23:51:20 -0500

    work on js functions

--- js/main.js
+++ js/main.js
@@ -1,14 +1,54 @@
-
+//define variables
 var teacher= "Mr. Vincent VanGogh ";
 var department= "Art";
-var rating= [2.1, 3.7, 4.9];
-var avgRating= (rating[0] +rating[1]+rating[2])/3;
-var roundedAvgRating= Math.round(10*avgRating)/10;
+var teacherRatings= [];
+var avgRating =0;
+//var avgRating= (rating[0] +rating[1]+rating[2])/3;
 
+//function to make an average of ratings with an array input
+function getRatingAvg(input)
+{
+  var ratingsSum=0;
+  for ( var i=0; i<input.length; i++)
+  {
+    ratingsSum += input[i];
+  }
+  avgRating= ratingsSum/input.length;
+  return avgRating;
+}
+
+//function to add a new rating into an array
+function addTeacherRating(someArray, newRating)
+{
+teacherRatings.push(newRating);
+return teacherRatings;
+}
+
+
+
+//take user rating input, add to rating array, and output  new average as alert
+var newRating= parseFloat(window.prompt("Please rate your professor between 0.0 - 5.0:"));
+
+while(newRating <= 0.0 || newRating >= 5.0)
+{
+var newRating= parseFloat(window.prompt("Please make your rating between 0.0 - 5.0:"));
+}
+
+addTeacherRating(teacherRatings, newRating);
+
+//define rounded variable to tenth place
+var roundedAvgRating= Math.round(10*getRatingAvg(teacherRatings))/10;
+
+alert("Thanks for rating " + teacher + " a " + newRating);
+
+//addTeacherRating(teacherRatings, newRating);
+alert(teacher + " now has an average rating of: " + roundedAvgRating);
+
+//print output of variables and ratings to console
 console.log("Teacher: ", teacher,
 "\nDepartment: " , department,
-"\nRatings: ", rating,
-"\n Avg Rating", roundedAvgRating);
+"\nRatings: ", teacherRatings,
+"\n Avg Rating: ", roundedAvgRating);
 
 var courses=[ "Interpersonal Communication 103", "Psychology 101", "Yoga 200"];
 var gpa= 4.0;
