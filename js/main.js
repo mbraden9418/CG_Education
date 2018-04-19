219f37d31f3949778d5306d75793f151eda59e0b
user <mbraden9418@yahoo.com>
2018-04-18 21:41:13 -0500

    finish js challenges 1 and 2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 //define variables
 var teacher= "Mr. Vincent VanGogh ";
 var department= "Art";
-var teacherRatings= [];
+var teacherRatings= [2.4, 3.6, 4.5];
 var avgRating =0;
-//var avgRating= (rating[0] +rating[1]+rating[2])/3;
 
 //function to make an average of ratings with an array input
 function getRatingAvg(input)
@@ -34,14 +33,15 @@ while(newRating <= 0.0 || newRating >= 5.0)
 var newRating= parseFloat(window.prompt("Please make your rating between 0.0 - 5.0:"));
 }
 
-addTeacherRating(teacherRatings, newRating);
+
 
 //define rounded variable to tenth place
-var roundedAvgRating= Math.round(10*getRatingAvg(teacherRatings))/10;
+
 
 alert("Thanks for rating " + teacher + " a " + newRating);
 
-//addTeacherRating(teacherRatings, newRating);
+addTeacherRating(teacherRatings, newRating);
+var roundedAvgRating= Math.round(10*getRatingAvg(teacherRatings))/10;
 alert(teacher + " now has an average rating of: " + roundedAvgRating);
 
 //print output of variables and ratings to console
@@ -50,7 +50,45 @@ console.log("Teacher: ", teacher,
 "\nRatings: ", teacherRatings,
 "\n Avg Rating: ", roundedAvgRating);
 
-var courses=[ "Interpersonal Communication 103", "Psychology 101", "Yoga 200"];
-var gpa= 4.0;
+var courses=[
+  ["Interpersonal Communication 103", "social sciences"],
+  ["Psychology 101", "social sciences"],
+  ["Yoga 200", "philosophy"],
+  ["Biochemistry 307", "science"],
+  ["HTML", "computer"],
+  ["CSS", "computer"],
+];
+
+console.log("Courses: " + courses);
+//var department= window.prompt("In which department are you searching for a course?").toLowerCase();
+
+var filteredCourses= [];
+
+function filterCourses(courses, department)
+{
+  for ( var i=0; i<courses.length; i++)
+  {
+    if (department==courses[i][1])
+    {
+      filteredCourses.push(courses[i][0]);
+    }
+  }
+}
+var department= window.prompt("In which department are you searching for a course?").toLowerCase();
+
+while (department != "science" && department != "computer" &&
+department != "art" && department != "language" &&
+ department != "philosophy" && department != "social sciences" )
+ {
+var department= window.prompt("Please enter a valid school department:").toLowerCase();
+}
+
+filterCourses(courses, department);
+alert("These courses are in the " + department + " department: " + filteredCourses);
+console.log("Courses: " + filteredCourses);
+
+
+
+/*var gpa= 4.0;
 var email= "lovinTrees@yahoo.com";
-console.log("Courses:", courses, "\n GPA:", gpa, "\n Email:", email);
+console.log("Courses:", courses, "\n GPA:", gpa, "\n Email:", email);*/
